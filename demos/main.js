d30cbf77981bd094c4fe547e7cb7299ffd018962
FionaDmello <40391218+FionaDmello@users.noreply.github.com>
2024-02-13 13:41:43 +0100

    ch: components part 1

--- demos/main.js
+++ demos/main.js
@@ -1,27 +1,12 @@
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
-const renderElement = () => {
-  const element = (
-    <div className="post">
-      <h1>My First Blog Post</h1>
-      <div>Author: Mark Twain</div>
-      <div>Published: {new Date().toLocaleTimeString()}</div>
-      <p>
-        I am new to blogging and this is my first post. You should expect a lot
-        of great things from me.
-      </p>
-    </div>
+function HelloWorld() {
+  return (
+    <div className="container">Hello World! This is a functional component</div>
   );
+}
 
-  root.render(element);
-};
+root.render(<HelloWorld />);
 
-setInterval(renderElement, 1000);
-
-/*
-NOTE: React Virtual DOM takes its current version and compares it to its new version every second
-It finds the diff and creates a patch stream based on it to be applied on the real DOM
-This patch is then applied on the real DOM to take it from its current version to the new version
-A current version of the virtual DOM is then created for the next update.
-*/
+/* NOTE: React component are always supposed to be PascalCased */
